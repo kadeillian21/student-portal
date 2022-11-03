444fb14c10700fff1b7c39e47eafe8086d7f2240
Rickie Natividad <rickienatividad85@gmail.com>
2022-11-03 13:58:01 -0400

    adds necessary .jsx files

---
+++ src/Login.jsx
@@ -0,0 +1,15 @@
+export function Login() {
+  const handleLogin = (event) =>
+  
+  return (
+    <div>
+      <form action="">
+        <div>Email</div>
+        <input type="email" />
+        <div>Password</div>
+        <input type="password" />
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
+}
