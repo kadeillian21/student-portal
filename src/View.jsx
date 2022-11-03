4be4aa051cb9647f6e422432203413aac307d8f1
Marcus Davis <marcusdavis1018@gmail.com>
2022-11-03 14:25:47 -0400

    hard coded views

--- src/View.jsx
+++ src/View.jsx
@@ -1,5 +1,45 @@
 import { useState, useEffect } from "react";
-import { Navbar } from "./Navbar";
-import { Update } from "./Update";
+// import { Navbar } from "./Navbar";
+// import { Update } from "./Update";
 import { Modal } from "./Modal";
-import { Login } from "./Login";
+// import { Login } from "./Login";
+
+export function View() {
+  return (
+    <div>
+      {/* <Login /> */}
+      <div className="index">
+        <h1> First Name </h1>
+        <h1> Last Name</h1>
+        <h3> Email</h3>
+        <p> Phone Number</p>
+        <p> Short Bio</p>
+        <p> Linkedin URL</p>
+        <p> Twitter Handle</p>
+        <p> Personal Blog/Website URL</p>
+        <p> Online Resume URL</p>
+        <p> Github URL</p>
+        <p> Will be a photo</p>
+        <h4> Experience </h4>
+        <p> Start Date </p>
+        <p> end date</p>
+        <p> job title</p>
+        <p> company name</p>
+        <p> details</p>
+        <h1> Education</h1>
+        <p> start date</p>
+        <p> end date</p>
+        <p> degree</p>
+        <p> university name</p>
+        <p> details </p>
+        <h1> Skills</h1>
+        <p> skill name</p>
+        <h1> Capstone</h1>
+        <p> name</p>
+        <p> description</p>
+        <p> URL </p>
+        <p> screenshot</p>
+      </div>
+    </div>
+  );
+}
