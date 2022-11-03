e3f78fe929a974b0a02209aef1af86d27f67b068
Rickie Natividad <rickienatividad85@gmail.com>
2022-11-03 14:35:39 -0400

    adds login form, axios post request to sessions, and future jwt attributes

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,13 +1,37 @@
+import axios from "axios";
+
+const jwt = localStorage.getItem("jwt");
+if (jwt) {
+  axios.defaults.headers.common["Authorization"] = `Bearer ${jwt}`;
+}
+
 export function Login() {
-  const handleLogin = (event) =>
-  
+  const handleLogin = (event) => {
+    event.preventDefault();
+    const params = new FormData(event.target);
+
+    axios
+      .post("http://localhost:3000/sessions.json", params)
+      .response((response) => {
+        console.log(response.data);
+        axios.defaults.headers.common["Authorization"] =
+          "Bearer " + response.data.jwt;
+        localStorage.setItem("jwt", response.data.jwt);
+        event.target.reset();
+        // window.location.href = "/"; REDIRECT?
+      })
+      .catch((error) => {
+        console.log(error.response.data.errors);
+      });
+  };
+
   return (
     <div>
-      <form action="">
+      <form onSubmit={handleLogin}>
         <div>Email</div>
-        <input type="email" />
+        <input type="email" name="email" />
         <div>Password</div>
-        <input type="password" />
+        <input type="password" name="password" />
         <button type="submit">Submit</button>
       </form>
     </div>
